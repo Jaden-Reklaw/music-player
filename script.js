39a816e2b6013ecb8d5551934fded7b1b915f2a6
Jordan Walker <jwalker117@gmail.com>
2020-09-16 11:20:47 -0500

    progess bar moving based off percentages of song completion using timeupdate event

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -27,6 +29,10 @@ const songs = [
         name: 'metric-1',
         displayName: 'Front Row (Remix)',
         artist: 'Metric/Jacinto Design',
+    },{
+        name: 'bouncin-ass',
+        displayName: 'Bouncin Ass',
+        artist: 'Vikstrom'
     }
 ];
 
@@ -86,5 +92,15 @@ const prevSong = () => {
     loadSong(songs[songIndex]);
     playSong();
 }
+
+// Update Progress Bar & Time
+const updateProgressBar = (e) => {
+    if(isPlaying) {
+        const {duration, currentTime} = e.srcElement;
+        const progressPercent = (currentTime/duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+    }
+}
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
